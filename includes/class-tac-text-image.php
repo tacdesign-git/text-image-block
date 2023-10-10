ba1f00313e74ed9491f4c176f0c638585d512301
tacdesign-git <dan@tac-design.co.uk>
2023-10-10 15:44:50 +0100

    dfgdfg

--- includes/class-tac-text-image.php
+++ includes/class-tac-text-image.php
@@ -179,7 +179,7 @@ class Tac_Text_Image {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 		//$this->loader->add_action('the_content', $plugin_public, 'inject_custom_content_block');
-		add_filter('the_content', array($this, 'inject_custom_content_block'));
+		
 	}
 
 	/**
