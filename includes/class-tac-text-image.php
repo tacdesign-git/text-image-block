ae6cc4027f54b5d6395c006d054b923bd11d524b
tacdesign-git <dan@tac-design.co.uk>
2023-10-10 15:57:54 +0100

    sdfsdf

--- includes/class-tac-text-image.php
+++ includes/class-tac-text-image.php
@@ -174,11 +174,12 @@ class Tac_Text_Image {
 	 */
 	private function define_public_hooks() {
 
-		$plugin_public = new Tac_Text_Image_Public( $this->get_plugin_name(), $this->get_version() $this->add_action('the_content', 'inject_custom_content_block'));
+		$plugin_public = new Tac_Text_Image_Public( $this->get_plugin_name(), $this->get_version());
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 		//$this->loader->add_action('the_content', $plugin_public, 'inject_custom_content_block');
+		$this->add_action('the_content', 'inject_custom_content_block', 20);
 		
 	}
 
