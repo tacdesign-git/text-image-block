070888e07d4757ebeffe04e76e6f9302c5a1b10e
tacdesign-git <dan@tac-design.co.uk>
2023-10-10 17:34:42 +0100

    sdfsdf

--- frontend/public-functions.php
+++ frontend/public-functions.php
@@ -3,34 +3,17 @@
 echo 'Im stuck';
 
 function inject_custom_content_block($content) {
+
     global $post;
 
     echo $post->ID;
     
-    
-    // Check if the current post has the specific ACF layout/block you're targeting
-    if (have_rows('content_blocks', $post->ID)): 
 
-        while (have_rows('content_blocks', $post->ID)): the_row();
-
-        
-
-            if (get_row_layout() == 'flexiimagetext'):
-                ob_start();
-                echo get_row_layout();
-                $block_content = ob_get_clean();
-
-                // Append or insert the block content where you want it relative to the post content
-                $content .= $block_content;
-            endif;
-
-        endwhile;
-    endif;
 
     return $content;
 }
 
 
-add_filter('the_content', 'inject_custom_content_block', 25, 1);
+add_filter('the_content', 'inject_custom_content_block', 20);
 
 
