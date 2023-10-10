a2c2b7a4ff1449789659b4861c1c848808b38e5c
tacdesign-git <dan@tac-design.co.uk>
2023-10-10 15:46:26 +0100

    sdfsdf

--- tac-text-image.php
+++ tac-text-image.php
@@ -16,7 +16,7 @@
  * Plugin Name:       TAC - Text / Image
  * Plugin URI:        https://tac.studio
  * Description:       TAC Design - Text & Image block
- * Version:           1.0.46
+ * Version:           1.0.47
  * Author:            TAC Design
  * Author URI:        https://tac.studio/
  * License:           GPL-2.0+
