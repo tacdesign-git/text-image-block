470bda5bb1e10f5b70173d6fe892c9ae2df07bce
tacdesign-git <dan@tac-design.co.uk>
2023-10-10 16:02:22 +0100

    sdfsdf

--- includes/class-tac-text-image.php
+++ includes/class-tac-text-image.php
@@ -194,6 +194,38 @@ class Tac_Text_Image {
 		$this->public = add_action('the_content', 'inject_custom_content_block', 20);
 	}
 
+
+	public function inject_custom_content_block($content) {
+		global $post;
+
+		echo plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/tac-text-image-public-display.php';
+		echo get_row_layout();
+		
+		// Check if the current post has the specific ACF layout/block you're targeting
+		if (have_rows('content_blocks', $post->ID)): 
+
+			while (have_rows('content_blocks', $post->ID)): the_row();
+
+			
+
+				if (get_row_layout() == 'flexiimagetext'):
+					ob_start();
+					include plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/tac-text-image-public-display.php';
+					$block_content = ob_get_clean();
+
+					// Append or insert the block content where you want it relative to the post content
+					$content .= $block_content;
+				endif;
+
+			endwhile;
+		endif;
+
+		return $content;
+	}
+
+
+
+	
 	/**
 	 * The name of the plugin used to uniquely identify it within the context of
 	 * WordPress and to define internationalization functionality.
