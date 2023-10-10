e3315d1c56de8b6e16274db2709ac62d95fc2477
tacdesign-git <dan@tac-design.co.uk>
2023-10-10 15:40:49 +0100

    sdfs

--- includes/class-tac-text-image.php
+++ includes/class-tac-text-image.php
@@ -178,40 +178,10 @@ class Tac_Text_Image {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-		$this->loader->add_filter('the_content', $plugin_public, 'inject_custom_content_block');
+		$this->loader->add_action('the_content', $plugin_public, 'inject_custom_content_block');
 
-		echo plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/tac-text-image-public-display.php';
 	}
 
-
-	public function inject_custom_content_block($content) {
-		global $post;
-
-		echo plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/tac-text-image-public-display.php';
-		echo get_row_layout();
-		
-		// Check if the current post has the specific ACF layout/block you're targeting
-		if (have_rows('content_blocks', $post->ID)): 
-
-			while (have_rows('content_blocks', $post->ID)): the_row();
-
-			
-
-				if (get_row_layout() == 'flexiimagetext'):
-					ob_start();
-					include plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/tac-text-image-public-display.php';
-					$block_content = ob_get_clean();
-
-					// Append or insert the block content where you want it relative to the post content
-					$content .= $block_content;
-				endif;
-
-			endwhile;
-		endif;
-
-		return $content;
-	}
-	
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
 	 *
