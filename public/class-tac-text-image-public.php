2797012b59a2138b3f12ba65321f3a747e20210d
tacdesign-git <dan@tac-design.co.uk>
2023-10-10 16:14:38 +0100

    fdgdfg

--- public/class-tac-text-image-public.php
+++ public/class-tac-text-image-public.php
@@ -101,35 +101,5 @@ class Tac_Text_Image_Public {
 	}
 
 
-	
-
-	public function inject_custom_content_block($content) {
-		global $post;
-
-		echo plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/tac-text-image-public-display.php';
-		echo get_row_layout();
-		
-		// Check if the current post has the specific ACF layout/block you're targeting
-		if (have_rows('content_blocks', $post->ID)): 
-
-			while (have_rows('content_blocks', $post->ID)): the_row();
-
-			
-
-				if (get_row_layout() == 'flexiimagetext'):
-					ob_start();
-					include plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/tac-text-image-public-display.php';
-					$block_content = ob_get_clean();
-
-					// Append or insert the block content where you want it relative to the post content
-					$content .= $block_content;
-				endif;
-
-			endwhile;
-		endif;
-
-		return $content;
-	}
-
 
 }
