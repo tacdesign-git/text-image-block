0274e3a256bcfcaf41bfdd266c4c68d93fe0cdf2
tacdesign-git <dan@tac-design.co.uk>
2023-10-11 14:27:25 +0100

    sdf

--- text-image-block.php
+++ text-image-block.php
@@ -4,7 +4,7 @@
  * @wordpress-plugin
  * Plugin Name:       TAC - Text / Image
  * Description:       TAC Design - Text & Image block
- * Version:           1.0.14
+ * Version:           1.0.15
  * Author:            TAC Design
 */
 
