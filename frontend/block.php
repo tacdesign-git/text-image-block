7d84800b40447f9b145e87e7407717a416c905ff
tacdesign-git <dan@tac-design.co.uk>
2023-10-11 15:21:37 +0100

    sdfsdf

--- frontend/block.php
+++ frontend/block.php
@@ -1,9 +1,7 @@
 <?php
-// File: my_block_template.php
 
-// Include the frontend functions file
 include plugin_dir_path(__FILE__) . 'frontend/public-functions.php';
 
 // Call the rendering function
-render_my_custom_block();
+//render_my_custom_block();
 
