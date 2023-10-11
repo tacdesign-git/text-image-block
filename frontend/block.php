3594c92653fd0c05e3958ee8c9c8127691b818f8
tacdesign-git <dan@tac-design.co.uk>
2023-10-11 15:40:11 +0100

    dfgdfg

--- frontend/block.php
+++ frontend/block.php
@@ -1,6 +1,6 @@
 <?php
 
-include plugin_dir_path(__FILE__) . 'frontend/public-functions.php';
+include plugin_dir_path(__FILE__) . 'public-functions.php';
 
 // Call the rendering function
 render_my_custom_block();
--- frontend/public-functions.php
+++ frontend/public-functions.php
@@ -7,8 +7,8 @@ function enqueue_my_custom_block_assets() {
         // Check if our block exists on the page before enqueuing
         while (have_rows('content_blocks')) : the_row();
             if (get_row_layout() == 'flexiimagetext') {
-                wp_enqueue_style('tac-block-style', plugin_dir_url(__FILE__) . 'frontend/public.css');
-                wp_enqueue_script('tac-block-script', plugin_dir_url(__FILE__) . 'frontend/public.js', array('jquery'), '', true);
+                wp_enqueue_style('tac-block-style', plugin_dir_url(__FILE__) . 'public.css');
+                wp_enqueue_script('tac-block-script', plugin_dir_url(__FILE__) . 'public.js', array('jquery'), '', true);
                 break; // Exit the loop once our block is found
             }
         endwhile;
