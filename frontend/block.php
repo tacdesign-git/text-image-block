3d507b120e4104dbaa7336aa44b89b090d9121f1
tacdesign-git <dan@tac-design.co.uk>
2023-10-29 10:57:10 +0000

    dfgdfg

--- frontend/block.php
+++ frontend/block.php
@@ -1,7 +1,32 @@
 <?php
 
-include plugin_dir_path(__FILE__) . 'public-functions.php';
+//include plugin_dir_path(__FILE__) . 'public-functions.php';
 
 // Call the rendering function
 //render_my_custom_block();
 
+function enqueue_my_custom_block_assets() {
+
+    if (have_rows('content_blocks')) {
+        // Check if our block exists on the page before enqueuing
+        while (have_rows('content_blocks')) : the_row();
+            if (get_row_layout() == 'flexiimagetext') {
+                wp_enqueue_style('tac-block-style', plugin_dir_url(__FILE__) . 'public.css');
+                wp_enqueue_script('tac-block-script', plugin_dir_url(__FILE__) . 'public.js', array('jquery'), '', true);
+
+                render_my_custom_block();
+                
+                break; // Exit the loop once our block is found
+            }
+        endwhile;
+    }
+
+}
+add_action('wp_enqueue_scripts', 'enqueue_my_custom_block_assets');
+
+// Render the block's content
+function render_my_custom_block() {
+    ob_start();
+    echo 'this a test...';
+    ob_end_flush();
+}
--- text-image-block.php
+++ text-image-block.php
@@ -4,7 +4,7 @@
  * @wordpress-plugin
  * Plugin Name:       TAC - Text / Image
  * Description:       TAC Design - Text & Image block
- * Version:           1.0.37
+ * Version:           1.0.38
  * Author:            TAC Design
 */
 
